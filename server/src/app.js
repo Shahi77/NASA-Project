49476eac2d9420efa1755547477ac67bf16f9c98
Shahi77 <shahiinfinity02@gmail.com>
2023-10-26 12:15:29 +0530

    GET planets method of api

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
-app.use(express.json());
 
-module.exports = app;
+// Cross Origin Communication
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = {
+  app,
+};
