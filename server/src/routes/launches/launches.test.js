770db7695c8ab5b6f48e3f605122fd60e2e272b0
Shahi77 <shahiinfinity02@gmail.com>
2023-10-27 20:55:09 +0530

    test: setting up API tests

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("It should respond with 200 success", () => {});
+
+  test("It should catch missing required properties", () => {});
+
+  test("It should also catch invalid dates", () => {});
+});
