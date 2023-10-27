ef08a99a36e6a93a50cadabcd25088c74fddf0df
Shahi77 <shahiinfinity02@gmail.com>
2023-10-27 21:07:04 +0530

    test: added GET /launches

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,11 @@
+const request = require("supertest");
+const { app } = require("../../app");
 describe("Test GET /launches", () => {
-  test("It should respond with 200 success", () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
 
