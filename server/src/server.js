d75fc3894b4165eddf547b6e22d5cc932e3a9753
Shahi77 <shahiinfinity02@gmail.com>
2023-10-29 21:19:33 +0530

    feat: connect with mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,28 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const { app } = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
+
+//dotenv.config({ path: "./config.env" });
+require("dotenv").config();
+const { DATABASE } = process.env;
+//const MONGO_URL = process.env.DATABASE;
+//console.log(process.env.DATABASE);
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(DATABASE);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
