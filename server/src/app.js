3e87446c6babdca474a19a291783ee066b551f51
Shahi77 <shahiinfinity02@gmail.com>
2023-12-16 11:28:48 +0530

    loading SpaceX data in our api

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const morgan = require("morgan");
-const v1 = require("./routes/version1");
+const api = require("./routes/version1");
 
 // Cross Origin Communication
 app.use(
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public"))); //middleware to serve frontend
 
 app.use(morgan("combined"));
-app.use("/v1", v1); //v1 api call
+app.use("v1", api); //v1 api call
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
--- server/src/routes/version1.js
+++ server/src/routes/version1.js
@@ -3,9 +3,9 @@ const express = require("express");
 const planetsRouter = require("./planets/planets.router");
 const launchesRouter = require("./launches/launches.router");
 
-const v1 = express.Router();
+const api = express.Router();
 
-v1.use("/planets", planetsRouter);
-v1.use("/launches", launchesRouter);
+api.use("/planets", planetsRouter);
+api.use("/launches", launchesRouter);
 
-module.exports = v1;
+module.exports = api;
